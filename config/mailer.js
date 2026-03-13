bbf61831af7188c956afcedd9d37d25d0b18d357
Celesta <cmamachan@4labsinc.com>
2026-03-13 17:28:08 +0530

    changes added

--- config/mailer.js
+++ config/mailer.js
@@ -15,14 +15,11 @@ const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
   secure: false,
+  family: 4, // force IPv4
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS
-  },
-  connectionTimeout: 10000,
-  greetingTimeout: 10000,
-  socketTimeout: 10000,
-  family: 4 // force IPv4 instead of IPv6
+  }
 });
 
 module.exports = transporter;
